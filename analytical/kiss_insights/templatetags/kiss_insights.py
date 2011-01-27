774970ec46ed9ae19752f85944ebcade5eef8f68
Joost Cassee <joost@cassee.net>
2011-01-27 12:51:29 +0100

    Refactor KISSinsights module into its own app
    This commit has not been tested.

--- analytical/kiss_insights/templatetags/kiss_insights.py
+++ analytical/kiss_insights/templatetags/kiss_insights.py
@@ -1,14 +1,14 @@
 """
-KISSinsights service.
+KISSinsights template tags.
 """
 
 import re
 
-from analytical.services.base import AnalyticalService
+from django.template import Library, Node, TemplateSyntaxError
 
 
-ACCOUNT_NUMBER_RE = re.compile(r'^\d{5}$')
-SITE_CODE_RE = re.compile(r'^[\d\w]{3}$')
+ACCOUNT_NUMBER_RE = re.compile(r'^\d+$')
+SITE_CODE_RE = re.compile(r'^[\w]{3}$')
 SETUP_CODE = """
     <script type="text/javascript">var _kiq = _kiq || []; %(commands)s</script>
     <script type="text/javascript" src="//s3.amazonaws.com/ki.js/%(account_number)s/%(site_code)s.js" async="true"></script>
@@ -17,15 +17,34 @@ IDENTIFY_CODE = "_kiq.push(['identify', '%s']);"
 SHOW_SURVEY_CODE = "_kiq.push(['showSurvey', %s]);"
 SHOW_SURVEY_CONTEXT_KEY = 'kiss_insights_show_survey'
 
-class KissInsightsService(AnalyticalService):
+
+register = Library()
+
+
+@register.tag
+def kiss_insights(parser, token):
+    """
+    KISSinsights set-up template tag.
+
+    Renders Javascript code to set-up surveys.  You must supply
+    your account number and site code in the
+    ``KISS_INSIGHTS_ACCOUNT_NUMBER`` and ``KISS_INSIGHTS_SITE_CODE``
+    settings.
+    """
+    bits = token.split_contents()
+    if len(bits) > 1:
+        raise TemplateSyntaxError("'%s' takes no arguments" % bits[0])
+    return KissInsightsNode()
+
+class KissInsightsNode(Node):
     def __init__(self):
         self.account_number = self.get_required_setting(
                 'KISS_INSIGHTS_ACCOUNT_NUMBER', ACCOUNT_NUMBER_RE,
-                "must be a string containing an five-digit number")
+                "must be (a string containing) a number")
         self.site_code = self.get_required_setting('KISS_INSIGHTS_SITE_CODE',
                 SITE_CODE_RE, "must be a string containing three characters")
 
-    def render_body_top(self, context):
+    def render(self, context):
         commands = []
         identity = self.get_identity(context)
         if identity is not None:
@@ -35,5 +54,6 @@ class KissInsightsService(AnalyticalService):
                     % context[SHOW_SURVEY_CONTEXT_KEY])
         except KeyError:
             pass
-        return SETUP_CODE % {'account_number': self.account_number,
+        html = SETUP_CODE % {'account_number': self.account_number,
                 'site_code': self.site_code, 'commands': " ".join(commands)}
+        return html
--- docs/services/kiss_insights.rst
+++ docs/services/kiss_insights.rst
@@ -1,35 +1,3 @@
-KISSinsights -- feedback surveys
-================================
+.. currentmodule:: analytical.kiss_insights
 
-KISSinsights_ provides unobtrusive surveys that pop up from the bottom
-right-hand corner of your website.  Asking specific questions gets you
-the targeted, actionable feedback you need to make your site better.
-
-.. _KISSinsights: http://www.kissinsights.com/
-
-The code is added to the top of the HTML body.  By default, the
-username of a logged-in user is passed to KISSinsights.  See
-:data:`ANALYTICAL_AUTO_IDENTIFY`.
-
-
-
-Required settings
------------------
-
-.. data:: KISSINSIGHTS_ACCOUNT_NUMBER
-
-  The KISSinsights account number::
-
-      KISSINSIGHTS_ACCOUNT_NUMBER = '12345'
-
-.. data:: KISSINSIGHTS_SITE_CODE
-
-  The KISSinsights website code::
-
-      KISSINSIGHTS_SITE_CODE = 'abc'
-
-You can find the account number and website code by visiting the code
-installation page of the website you want to place the surveys on.  You
-will see some HTML code with a Javascript tag with a ``src`` attribute
-containing ``//s3.amazonaws.com/ki.js/XXXXX/YYY.js``.  Here ``XXXXX`` is
-the account number and ``YYY`` the website code.
+.. automodule:: analytical.kiss_insights
