1941af976a8f8479259a01c35de848b9b34bbd7d
Joost Cassee <joost@cassee.net>
2011-01-30 10:35:42 +0100

    Bumped version to 0.2.0

--- analytical/__init__.py
+++ analytical/__init__.py
@@ -10,6 +10,6 @@ Django_ project.  See the ``docs`` directory for more information.
 
 __author__ = "Joost Cassee"
 __email__ = "joost@cassee.net"
-__version__ = "0.2.0alpha"
+__version__ = "0.2.0"
 __copyright__ = "Copyright (C) 2011 Joost Cassee"
 __license__ = "MIT License"
