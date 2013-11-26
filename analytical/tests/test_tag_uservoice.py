010612b11582a78bddaacaccc7f6166c58222f6b
Martín Gaitán <gaitan@gmail.com>
2013-11-26 14:49:45 -0300

    update tests

--- analytical/tests/test_tag_uservoice.py
+++ analytical/tests/test_tag_uservoice.py
@@ -20,13 +20,11 @@ class UserVoiceTagTestCase(TagTestCase):
 
     def test_node(self):
         r = UserVoiceNode().render(Context())
-        self.assertTrue("'widget.uservoice.com/abcdefghijklmnopqrst.js'" in r,
-                r)
+        self.assertIn("widget.uservoice.com/abcdefghijklmnopqrst.js", r)
 
     def test_tag(self):
         r = self.render_tag('uservoice', 'uservoice')
-        self.assertTrue("'widget.uservoice.com/abcdefghijklmnopqrst.js'" in r,
-                r)
+        self.assertIn("widget.uservoice.com/abcdefghijklmnopqrst.js", r)
 
     @override_settings(USERVOICE_WIDGET_KEY=SETTING_DELETED)
     def test_no_key(self):
@@ -39,31 +37,29 @@ class UserVoiceTagTestCase(TagTestCase):
     @override_settings(USERVOICE_WIDGET_KEY='')
     def test_empty_key(self):
         r = UserVoiceNode().render(Context())
-        self.assertFalse("widget.uservoice.com" in r, r)
+        self.assertNotIn("widget.uservoice.com", r)
 
     @override_settings(USERVOICE_WIDGET_KEY='')
     def test_overridden_empty_key(self):
         vars = {'uservoice_widget_key': 'bcdefghijklmnopqrstu'}
         r = UserVoiceNode().render(Context(vars))
-        self.assertTrue("'widget.uservoice.com/bcdefghijklmnopqrstu.js'" in r,
-                r)
+        self.assertIn("widget.uservoice.com/bcdefghijklmnopqrstu.js", r)
 
     def test_overridden_key(self):
         vars = {'uservoice_widget_key': 'defghijklmnopqrstuvw'}
         r = UserVoiceNode().render(Context(vars))
-        self.assertTrue("'widget.uservoice.com/defghijklmnopqrstuvw.js'" in r,
-                r)
+        self.assertIn("widget.uservoice.com/defghijklmnopqrstuvw.js", r)
 
     @override_settings(USERVOICE_WIDGET_OPTIONS={'key1': 'val1'})
     def test_options(self):
         r = UserVoiceNode().render(Context())
-        self.assertTrue("UserVoice.push(['set', {'key1': 'val1'}]);" in r, r)
+        self.assertIn("""UserVoice.push(['set', {"key1": "val1"}]);""", r)
 
     @override_settings(USERVOICE_WIDGET_OPTIONS={'key1': 'val1'})
     def test_override_options(self):
         data = {'uservoice_widget_options': {'key1': 'val2'}}
         r = UserVoiceNode().render(Context(data))
-        self.assertTrue("UserVoice.push(['set', {'key1': 'val2'}]);" in r, r)
+        self.assertIn("""UserVoice.push(['set', {"key1": "val2"}]);""", r)
 
     def test_auto_trigger(self):
         r = UserVoiceNode().render(Context())
