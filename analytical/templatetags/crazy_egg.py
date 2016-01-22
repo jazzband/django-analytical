ea6b49f354584069e15c3f8b3f7151dacf7354cf
Peter Bittner <django@bittner.it>
2016-01-23 00:04:16 +0100

    [Crazy Egg] Fix code smell "redefine built-in" and flake8 complaints

--- analytical/templatetags/crazy_egg.py
+++ analytical/templatetags/crazy_egg.py
@@ -12,7 +12,10 @@ from analytical.utils import is_internal_ip, disable_html, get_required_setting
 
 
 ACCOUNT_NUMBER_RE = re.compile(r'^\d+$')
-SETUP_CODE = """<script type="text/javascript" src="//dnn506yrbagrg.cloudfront.net/pages/scripts/%(account_nr_1)s/%(account_nr_2)s.js"></script>"""
+SETUP_CODE = '<script type="text/javascript" src="{placeholder_url}">' \
+             '</script>'.\
+    format(placeholder_url='//dnn506yrbagrg.cloudfront.net/pages/scripts/'
+                           '%(account_nr_1)s/%(account_nr_2)s.js')
 USERVAR_CODE = "CE2.set(%(varnr)d, '%(value)s');"
 
 
@@ -36,19 +39,25 @@ def crazy_egg(parser, token):
 
 class CrazyEggNode(Node):
     def __init__(self):
-        self.account_nr = get_required_setting('CRAZY_EGG_ACCOUNT_NUMBER',
-                ACCOUNT_NUMBER_RE, "must be (a string containing) a number")
+        self.account_nr = get_required_setting(
+            'CRAZY_EGG_ACCOUNT_NUMBER',
+            ACCOUNT_NUMBER_RE, "must be (a string containing) a number"
+        )
 
     def render(self, context):
-        html = SETUP_CODE % {'account_nr_1': self.account_nr[:4],
-            'account_nr_2': self.account_nr[4:]}
+        html = SETUP_CODE % {
+            'account_nr_1': self.account_nr[:4],
+            'account_nr_2': self.account_nr[4:],
+        }
         values = (context.get('crazy_egg_var%d' % i) for i in range(1, 6))
-        vars = [(i, v) for i, v in enumerate(values, 1) if v is not None]
-        if vars:
-            js = " ".join(USERVAR_CODE % {'varnr': varnr, 'value': value}
-                        for (varnr, value) in vars)
-            html = '%s\n<script type="text/javascript">%s</script>' \
-                    % (html, js)
+        params = [(i, v) for i, v in enumerate(values, 1) if v is not None]
+        if params:
+            js = " ".join(USERVAR_CODE % {
+                'varnr': varnr,
+                'value': value,
+            } for (varnr, value) in params)
+            html = '%s\n' \
+                   '<script type="text/javascript">%s</script>' % (html, js)
         if is_internal_ip(context, 'CRAZY_EGG'):
             html = disable_html(html, 'Crazy Egg')
         return html
