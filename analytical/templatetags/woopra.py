109de4c1d738571c1934df9ce80af7bf37a30c79
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-05-25 10:43:11 -0700

    removes crud per PR comments

--- analytical/templatetags/woopra.py
+++ analytical/templatetags/woopra.py
@@ -24,13 +24,8 @@ TRACKING_CODE = """
       var woo_settings = %(settings)s;
       var woo_visitor = %(visitor)s;
       !function(){var a,b,c,d=window,e=document,f=arguments,g="script",h=["config","track","trackForm","trackClick","identify","visit","push","call"],i=function(){var a,b=this,c=function(a){b[a]=function(){return b._e.push([a].concat(Array.prototype.slice.call(arguments,0))),b}};for(b._e=[],a=0;a<h.length;a++)c(h[a])};for(d.__woo=d.__woo||{},a=0;a<f.length;a++)d.__woo[f[a]]=d[f[a]]=d[f[a]]||new i;b=e.createElement(g),b.async=1,b.src="//static.woopra.com/js/w.js",c=e.getElementsByTagName(g)[0],c.parentNode.insertBefore(b,c)}("woopra");
-
-      // configure tracker
       woopra.config(woo_settings);
-
       woopra.identify(woo_visitor);
-
-      // track pageview
       woopra.track();
     </script>
 """
