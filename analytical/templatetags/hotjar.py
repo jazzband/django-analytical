00bc123b88173f3753270eec47e51b49509c0f7c
Peter Bittner <django@bittner.it>
2020-12-05 16:22:32 +0100

    Reduce indentation of Hotjar JS code

--- analytical/templatetags/hotjar.py
+++ analytical/templatetags/hotjar.py
@@ -11,14 +11,14 @@ from analytical.utils import get_required_setting, is_internal_ip, disable_html
 
 HOTJAR_TRACKING_CODE = """\
 <script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:%(HOTJAR_SITE_ID)s,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:%(HOTJAR_SITE_ID)s,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
 """
 
