8de164aaefcd3433bf251d395ce8a3b981bb8b57
Peter Bittner <django@bittner.it>
2020-12-05 16:22:32 +0100

    Reduce indentation of Hotjar JS code

--- analytical/tests/test_tag_hotjar.py
+++ analytical/tests/test_tag_hotjar.py
@@ -13,14 +13,14 @@ from analytical.utils import AnalyticalException
 
 expected_html = """\
 <script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:123456789,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:123456789,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
 """
 
