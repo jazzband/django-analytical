1a25ce1ac310cf3c3825b81b423037b668057114
Peter Bittner <django@bittner.it>
2020-12-06 15:06:34 +0100

    Hard-require Python 3.6+, add link to docs on PyPI

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setup(
         'analytical',
         'analytical.templatetags',
     ],
-    keywords=['django', 'analytics'],
+    keywords=[
+        'django',
+        'analytics',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -34,15 +37,19 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    python_requires='>=3.6',
     platforms=['any'],
     url='https://github.com/jazzband/django-analytical',
     download_url='https://github.com/jazzband/django-analytical/archive/master.zip',
+    project_urls={
+        'Documentation': 'https://django-analytical.readthedocs.io/',
+    },
 )
