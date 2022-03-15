844a178c0480bf1e7ce6b6447dd80daa96039659
Peter Bittner <django@bittner.it>
2022-03-15 09:27:31 +0100

    Simplify copyright notice
    Co-authored-by: David Smith <39445562+smithdc1@users.noreply.github.com>

--- analytical/__init__.py
+++ analytical/__init__.py
@@ -5,4 +5,4 @@ Analytics service integration for Django projects
 __author__ = "Joost Cassee"
 __email__ = "joost@cassee.net"
 __version__ = "3.1.0"
-__copyright__ = "Copyright (C) 2011-2022 Joost Cassee and contributors"
+__copyright__ = "Copyright (C) 2011 Joost Cassee and contributors"
