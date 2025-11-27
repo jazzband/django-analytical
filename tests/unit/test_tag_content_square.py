8f10210ec4198bb2f223c0c9da68c72bf9678604
basel <basel.mahmoud@heron-ai.com>
2025-11-27 02:21:57 +0200

    add the actuall values in the expected string

--- tests/unit/test_tag_content_square.py
+++ tests/unit/test_tag_content_square.py
@@ -15,13 +15,13 @@ expected_html = """\
 <script>
     (function (c, s, q, u, a, r, e) {
         c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-        c._hjSettings = { hjid: a };
-        r = s.getElementsByTagName('head')[0];
-        e = s.createElement('script');
-        e.async = true;
-        e.src = q + c._hjSettings.hjid + u;
+        c._hjSettings={hjid:123456789 };
+        r=s.getElementsByTagName('head')[0];
+        e=s.createElement('script');
+        e.async=true;
+        e.src=q+c._hjSettings.hjid+u;
         r.appendChild(e);
-    })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', %(CONTENTSQUARE_SITE_ID)s);
+    })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 123456789);
 </script>
 """
 
